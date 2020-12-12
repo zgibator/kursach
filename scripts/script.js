1a625129201047539e1fda6cd2c890e001db4d0c
zgibator <belyakvv2001@gmail.com>
2020-12-12 12:49:15 +0300

    gabella

--- scripts/script.js
+++ scripts/script.js
@@ -26,14 +26,22 @@ var app_1 = new Vue({
         rated: "img/rated.png",
         unrated: "img/unrated.png",
         counter: 0,
+        search: '',
+        isActive: true,
         imageRate: '',
-        t: 0,
+        counterOfGood: 1,
+        price: 0,
+        totalPrice: 0,
         a: '',
         p: 3,
         discountPrice: 0,
+        cart: [
+            { id: 1, codeOfGood: 1, countOfGood: 2 }
+        ],
+        goodItem: [],
         itemGood: [
-            { name: "ЭЛЕКТРОГИТАРА J8 ST HMG 101 BK", codeOfGood: 1, rate: 4, comment: 3, price: 2772, discount: 10, image1: 'img/guitar.jpg', date: "12.29.2019" },
-            { name: "ЭЛЕКТРОГИТАРА J8 ST HMG 101 BK", codeOfGood: 2, rate: 4, comment: 3, price: 2772, discount: 10, image1: 'img/guitar2.jpg', date: '11.23.2021' },
+            { name: "ЭЛЕКТРОГИТАРА J8 ST HMG 101 BK", description: 'Гитара J8 ST HMG 101 BK электро ,сочетает в себе все,что нужно для электрогитары. Стандартная гитара типа Stratocaster. Отлично подойдет для скоростной техники игры.', specific: '-Корпус: ольха', proizvoditel: 'j8', codeOfGood: 1, rate: 4, comment: 3, price: 3772, discount: 39, image2: ['img/guitar.jpg', 'img/guitar.jpg', 'img/guitar.jpg', 'img/guitar.jpg', 'img/guitar.jpg', 'img/guitar2.jpg', 'img/guitar3.jpg', 'img/guitar4.jpg'], image1: 'img/guitar2.jpg', image3: 'img/guitar3.jpg', image4: 'img/guitar4.jpg', date: "12.29.2019" },
+            { name: "Барабан J8 ST HMG 101 BK", codeOfGood: 2, rate: 4, comment: 3, price: 2772, discount: 10, image1: 'img/guitar2.jpg', date: '11.23.2021' },
             { name: "ЭЛЕКТРОГИТАРА J8 ST HMG 101 BK", codeOfGood: 3, rate: 4, comment: 3, price: 2772, discount: 10, image1: 'img/guitar3.jpg', date: '11.23.2021' },
             { name: "ЭЛЕКТРОГИТАРА J8 ST HMG 101 BK", codeOfGood: 3, rate: 4, comment: 3, price: 2772, discount: 10, image1: 'img/guitar4.jpg', date: '11.23.2021' },
             { name: "ЭЛЕКТРОГИТАРА J8 ST HMG 101 BK", codeOfGood: 3, rate: 4, comment: 3, price: 2772, discount: 10, image1: 'img/good1.png', date: '11.23.2021' },
@@ -54,6 +62,15 @@ var app_1 = new Vue({
         ],
     },
     methods: {
+        show_popup() {
+            let el = document.getElementById('add_success');
+            el.className = 'popup_add_success popup_add_success_open';
+            setTimeout(() => el.classList.remove('popup_add_success_open'), 5000);
+        },
+        addToCart(item, count) {
+            this.cart.push({ id: this.cart.length + 1, codeOfGood: item.codeOfGood, countOfGood: count });
+            this.show_popup();
+        },
         rating(p, g) {
             if (p <= g.rate) {
                 return this.imageRate = this.rated;
@@ -95,6 +112,40 @@ var app_1 = new Vue({
             }
             return s;
         },
+        animScrollImgUp(a) {
+            var animat = document.getElementById(a);
+            function draw(timePassed) {
+                animat.scrollBy(0, -2);
+            }
+            let start = Date.now();
+            let timer = setInterval(function () {
+                let timePassed = Date.now() - start;
+
+                if (timePassed >= 360) {
+                    clearInterval(timer);
+                    return;
+                }
+                draw(timePassed);
+
+            }, 10);
+        },
+        animScrollImgDown(a) {
+            var animat = document.getElementById(a);
+            function draw(timePassed) {
+                animat.scrollBy(0, 2);
+            }
+            let start = Date.now();
+            let timer = setInterval(function () {
+                let timePassed = Date.now() - start;
+
+                if (timePassed >= 370) {
+                    clearInterval(timer);
+                    return;
+                }
+                draw(timePassed);
+
+            }, 10);
+        },
         anim3(a) {
             var animat = document.getElementById(a);
             function draw(timePassed) {
@@ -129,20 +180,54 @@ var app_1 = new Vue({
 
             }, 15);
         },
-        scr() {
-            const anchors = document.querySelectorAll('a[href*="#"]')
+        countPlus() {
+            console.log('lox');
+            return this.counterOfGood++;
+        }
+    },
+    watch: {
 
-            for (let anchor of anchors) {
-                anchor.addEventListener('click', function (e) {
-                    e.preventDefault()
 
-                    const blockID = anchor.getAttribute('href').substr(1)
 
-                    document.getElementById(blockID).scrollIntoView({
-                        behavior: 'smooth',
-                        block: 'start'
-                    })
-                })
+        search: function (newSearch) {
+            if (newSearch.length > 1) {
+                this.isActive = false;
+                for (let i = 0; i < this.itemGood.length; i++) {
+                    if (this.itemGood[i].name.toLowerCase().includes(newSearch.toLowerCase())) {
+                        this.goodItem[this.counter] = this.itemGood[i];
+                        console.log(this.goodItem[0]);
+                        console.log(this.itemGood.length);
+                        this.counter++;
+                    }
+                }
+            } else {
+                this.goodItem = [];
+                this.isActive = true;
+            }
+        }
+
+    }
+});
+var app_2 = new Vue({
+    el: '#app_2',
+    data: {
+        search: '',
+    },
+    watch: {
+        search: function (newSearch) {
+            if (newSearch.length > 1) {
+                app_1.isActive = false;
+                for (let i = 0; i < this.itemGood.length; i++) {
+                    if (app_1.itemGood[i].name.toLowerCase().includes(newSearch.toLowerCase())) {
+                        app_1.goodItem[app_1.counter] = app_1.itemGood[i];
+                        console.log(app_1.goodItem[0]);
+                        console.log(app_1.itemGood.length);
+                        app_1.counter++;
+                    }
+                }
+            } else {
+                app_1.goodItem = [];
+                app_1.isActive = true;
             }
         }
     }
